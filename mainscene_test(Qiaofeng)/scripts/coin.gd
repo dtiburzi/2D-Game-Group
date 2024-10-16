33d63a00e7e6ecd0de89de0ea33e39df3aac44a5
LEADGREY <august60@126.com>
2024-10-15 22:13:58 -0400

    level1！

--- mainscene_test(Qiaofeng)/scripts/coin.gd
+++ mainscene_test(Qiaofeng)/scripts/coin.gd
@@ -3,5 +3,5 @@ extends Area2D
 @onready var game_manager: Node = %"Game manager"
 
 func _on_body_entered(_body):
-	#game_manager.add_point()
+	game_manager.add_point()
 	queue_free()
--- mainscene_test(Qiaofeng)/scripts/killzone.gd
+++ mainscene_test(Qiaofeng)/scripts/killzone.gd
@@ -3,8 +3,7 @@ extends Area2D
 @onready var timer: Timer = $Timer
 
 func _on_body_entered(_body: Node2D) -> void:
-	print("you died")
-	_body.get_node("CollisionShape2D").queue_free()
+	print("you died!")
 	timer.start()
 
 func _on_timer_timeout() -> void:
