e36b1548cd2a743b66e6ffa6a1ed02bab25b7e6f
LEADGREY <august60@126.com>
2024-10-26 18:37:35 -0400

    label (incomplete)

--- mainscene_test(version 10.26)/scripts/game_manager.gd
+++ mainscene_test(version 10.26)/scripts/game_manager.gd
@@ -1,7 +1,9 @@
 extends Node
 
 var score = 0
+@onready var score_label: Label = $"../Player_test/Camera2D/Score_Label"
 
 func add_point():
 	score += 1
 	print(score)
+	#score_label.text = str(score)+" / 15"
